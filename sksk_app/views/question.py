3d91956b3344e27aff538ddb83c475c858662bf8
yurio <wwt.yuri@gmail.com>
2023-02-26 06:16:31 +0900

    [add]項目ごとの間違えた問題の復習
    Close #76

--- sksk_app/views/question.py
+++ sksk_app/views/question.py
@@ -58,35 +58,39 @@ def select_element():
 def show_first_question():
     element_id = request.args.get('e')
     guest = int(request.args.get('guest'))
+    review = int(request.args.get('review'))
     no = 1
 
-    if guest:
-        questions_raw = Question.query.filter(Question.element==element_id).filter(Question.released==1).order_by(Question.position.asc()).limit(5).all()
+    if not review:
+        if guest:
+            questions_raw = Question.query.filter(Question.element==element_id).filter(Question.released==1).order_by(Question.position.asc()).limit(5).all()
 
-    else:
-        user_id = session['user_id']
-        #1周目の問題の取得(5問)
-        answered_question = Score.query.with_entities(Score.question).filter(Score.user==user_id)
-        questions_raw = Question.query.filter(Question.element==element_id).filter(Question.id.not_in(answered_question)).order_by(Question.id.asc()).limit(5).all()
+        else:
+            user_id = session['user_id']
+            #1周目の問題の取得(5問)
+            answered_question = Score.query.with_entities(Score.question).filter(Score.user==user_id)
+            questions_raw = Question.query.filter(Question.element==element_id).filter(Question.id.not_in(answered_question)).filter(Question.released==1).order_by(Question.id.asc()).limit(5).all()
 
-        if not questions_raw:
-            #2周目以降の問題を取得(5問)
-            questions_raw = Score.query.with_entities(Question.id,func.count(Score.question).label('count')).join(Question).filter(Question.element==element_id).filter(Score.user==user_id).group_by(Score.question).order_by('count', func.lpad((Question.id), 6, 0)).limit(5).all()
+            if not questions_raw:
+                #2周目以降の問題を取得(5問)
+                questions_raw = Score.query.with_entities(Question.id,func.count(Score.question).label('count')).join(Question).filter(Question.element==element_id).filter(Score.user==user_id).group_by(Score.question).order_by('count', func.lpad((Question.id), 6, 0)).limit(5).all()
 
-    questions = []
-    for question in questions_raw:
-        questions.append([question.id, 0])
-    session['questions'] = questions
+        questions = []
+        for question in questions_raw:
+            questions.append([question.id, 0])
+        session['questions'] = questions
+    questions = session.get('questions')
 
     question = editor.QuestionManager.fetch_question_with_hints(questions[0][0])
     attribute = editor.QuestionManager.fetch_attribute(question['element'])
 
-    return render_template('question/question.html', question=question, no=no, attribute=attribute, guest=guest)
+    return render_template('question/question.html', question=question, no=no, attribute=attribute, guest=guest, review=review)
 
 @question.route('/record_temp', methods=['POST'])
 def record_score_temp():
     guest = int(request.form['guest'])
     no = int(request.form['no'])
+    review = int(request.form['review'])
     correct = int(request.form['correct'])
     questions = session.get('questions')
 
@@ -95,15 +99,16 @@ def record_score_temp():
     session['questions'] = questions
 
     if no < len(questions):
-        return redirect(url_for('question.show_question', no=no, guest=guest), code=307)
+        return redirect(url_for('question.show_question', no=no, guest=guest, review=review), code=307)
     else:
-        return redirect(url_for('question.finish', questions=questions, no=no, guest=guest), code=307)
+        return redirect(url_for('question.finish', questions=questions, no=no, guest=guest, review=review), code=307)
 
 @question.route('/question/record', methods=['POST'])
 @login_required
 def record_score():
     guest = int(request.form['guest'])
     no = int(request.form['no'])
+    review = int(request.form['review'])
     correct = int(request.form['correct'])
     questions = session.get('questions')
     user_id = session.get('user_id')
@@ -115,26 +120,28 @@ def record_score():
     user_setting.ScoreManager.add_score(user_id, questions[index][0], correct, 0)
     
     if no < len(questions):
-        return redirect(url_for('question.show_question', no=no, guest=guest), code=307)
+        return redirect(url_for('question.show_question', no=no, guest=guest, review=review), code=307)
     else:
-        return redirect(url_for('question.finish', questions=questions, no=no, guest=guest), code=307)
+        return redirect(url_for('question.finish', questions=questions, no=no, guest=guest, review=review), code=307)
     
 @question.route('/question', methods=['POST'])
 def show_question():
     guest = int(request.form['guest'])
     no = int(request.form['no'])
+    review = int(request.form['review'])
     questions = session.get('questions')
     question = editor.QuestionManager.fetch_question_with_hints(questions[no][0])
     no += 1
 
     attribute = editor.QuestionManager.fetch_attribute(question['element'])
 
-    return render_template('question/question.html', question=question, no=no, attribute=attribute, guest=guest)
+    return render_template('question/question.html', question=question, no=no, attribute=attribute, guest=guest, review=review)
 
 @question.route('/question/finish', methods=['POST'])
 def finish():
     guest = int(request.form['guest'])
     no = int(request.form['no'])
+    review = int(request.form['review'])
     questions = session.get('questions')
 
     correct_answer = 0
@@ -150,6 +157,21 @@ def finish():
 
     next_element = Element.query.join(Question).filter(Element.e_group==element.e_group).filter(Element.position > element.position).filter(not_(Element.id==question.element)).filter(Question.released==1).first()
 
-    return render_template('question/finish.html', no=no, correct_answer=correct_answer, correct_ratio=correct_ratio, guest=guest, next_element=next_element) 
+    return render_template('question/finish.html', no=no, correct_answer=correct_answer, correct_ratio=correct_ratio, guest=guest, next_element=next_element, review=review) 
+
+@question.route('/review/element', methods=['GET'])
+def start_review_element():
+    guest = int(request.args.get('guest'))
+    answered_questions = session.get('questions')
+    questions = []
+    for answered_question in answered_questions:
+        if answered_question[1] == 0:
+            questions.append([answered_question[0], 0])
+
+    session['questions'] = questions
+    review = 1
+    # question = db.session.get(Question, questions[0][0])
+
+    return redirect(url_for('question.show_first_question', e=1, guest=guest, review=review))
 
 
