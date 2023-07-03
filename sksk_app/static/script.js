c6c345e2205e1b8bf197da8daaf2c9d5f1bb580d
yurio <wwt.yuri@gmail.com>
2023-07-03 19:46:16 +0900

    [update]ハンバーガーメニューをクリックで解除
    jQueryを導入しました。
    
    Close #198

---
+++ sksk_app/static/script.js
@@ -0,0 +1,8 @@
+'use strict';
+$(document).ready(function () { 
+    $(document).click(function () {
+       // if($(".navbar-collapse").hasClass("in")){
+         $('.navbar-collapse').collapse('hide');
+       // }
+    });
+  });
